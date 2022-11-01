6e7c9aaa443d28507f0a3929b1a6d4b955581e4c
Nik <sch-nikolaj@yandex.ru>
2022-11-02 01:30:48 +0300

    fix bugs

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class TelegramID(models.Model):
-    telegram_id = models.CharField('Telegram id', max_length=100, unique=False)
+    telegram_id = models.CharField('Telegram id', max_length=100)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='telegram_id')
 
 
