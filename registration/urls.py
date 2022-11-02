d7fedfb3a7c92b1c1a6ba9b44963de1b0a4251a8
Nik <sch-nikolaj@yandex.ru>
2022-11-02 10:05:44 +0300

    fix

--- registration/urls.py
+++ registration/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from registration.views import IndexView, BotUrlView
 from users.views import LoginUserView, LogoutUserView
 
